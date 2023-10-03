e97597c3cc30bbbf2592de5b5925a34b2f479c13
Joseph <j.t.cartoonist@gmail.com>
2023-10-03 17:40:39 +0100

    Initial commit

---
+++ app/src/main/java/com/tutorials/kodecampcurrencyconverter/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.tutorials.kodecampcurrencyconverter
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.tutorials.kodecampcurrencyconverter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.convertButton.setOnClickListener {
+            convertCurrency()
+        }
+    }
+
+
+    private fun convertCurrency(){
+        val currency = binding.currencyToConvertEditText.text.toString().toDoubleOrNull()
+
+        currency?.let {
+            val checkedCurrency = binding.currencyConversionOptions.checkedRadioButtonId
+            // these values were gotten from google market summary as at 03/10/2023 - 4:55pm
+            val currencyToConvertToValue=when(checkedCurrency){
+                R.id.dollar_conversion_option->{
+                    767.23
+                }
+                R.id.pounds_conversion_option->{
+                    925.89
+                }
+                R.id.euro_conversion_option->{
+                    802.04
+                }
+                else->{
+                    20_946_955.67
+                }
+            }
+            val currencyToConvertToSymbol = when (checkedCurrency) {
+                R.id.dollar_conversion_option -> {
+                    "$"
+                }
+
+                R.id.pounds_conversion_option -> {
+                   "£"
+                }
+
+                R.id.euro_conversion_option -> {
+                    "€"
+                }
+
+                else -> {
+                    "₿"
+                }
+            }
+
+            var newValue = it * currencyToConvertToValue
+            if (binding.roundUpSwitch.isChecked) {
+                newValue = kotlin.math.ceil(newValue)
+            }
+
+            binding.conversionResultText.text = getString(
+                R.string.conversion_result_text,
+                currencyToConvertToSymbol,
+                newValue.toString(),
+                currencyToConvertToValue.toString()
+            )
+        } ?: emptyStateValue()
+    }
+
+    private fun emptyStateValue(){
+        Toast.makeText(this, "Invalid conversion value", Toast.LENGTH_SHORT).show()
+        return
+    }
+}
